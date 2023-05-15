61a126a3c5afd7c83be133cee1e1245d9cc1b400
Ryan-Zayne <phoenixastro97@gmail.com>
2023-05-15 11:47:18 +0100

    In this commit, I tested how to use js to disable mouse wheel scroll in zustand

--- src/store/zustand/slices/globalStateSlice.js
+++ src/store/zustand/slices/globalStateSlice.js
@@ -1,23 +1,48 @@
+import { scrollbarWidth } from '../../../utils/globalVariables';
+
 export const createGlobalStateSlice = (set, get) => ({
 	isNavShow: false,
 	isSearchShow: false,
 	currentSlide: 0,
+
 	globalActions: {
-		toggleNavShow: () => set((state) => ({ isNavShow: !state.isNavShow })),
-		handleNoScrollOnNavSHow: () => {
+		preventScroll: (event) => {
+			event.preventDefault();
+		},
+
+		toggleNavShow: () => {
+			set((state) => ({ isNavShow: !state.isNavShow }));
+
 			if (get().isNavShow) {
+				document.body.style.setProperty('--scrollbar-padding', ` ${scrollbarWidth}rem`);
 				document.body.classList.add('overflow-hidden');
+				document.body.addEventListener('wheel', get().globalActions.preventScroll, {
+					passive: false,
+				});
 			} else {
+				document.body.style.setProperty('--scrollbar-padding', '');
 				document.body.classList.remove('overflow-hidden');
+				document.body.removeEventListener('wheel', get().globalActions.preventScroll, {
+					passive: false,
+				});
 			}
 		},
+
 		closeNavShow: () => {
 			set({ isNavShow: false });
+			document.body.style.setProperty('--scrollbar-padding', '');
 			document.body.classList.remove('overflow-hidden');
+			document.body.removeEventListener('wheel', get().globalActions.preventScroll, {
+				passive: false,
+			});
 		},
+
 		toggleSearchShow: () => set((state) => ({ isSearchShow: !state.isSearchShow })),
+
 		nextSlide: () => set((state) => ({ currentSlide: state.currentSlide + 1 })),
+
 		previousSlide: () => set((state) => ({ currentSlide: state.currentSlide - 1 })),
+
 		goToSlide: (resetValue) => set({ currentSlide: resetValue }),
 	},
 });
