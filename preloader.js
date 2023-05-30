10d42dc15fea8a40b9dae79a60d4c128a30f6cb9
Ryan-Zayne <phoenixastro97@gmail.com>
2023-05-30 09:42:40 +0100

    .

--- preloader.js
+++ preloader.js
@@ -21,4 +21,4 @@ const handleLoaderRemoval = () => {
 
 window.addEventListener('DOMContentLoaded', handleLoaderRemoval);
 
-window.history.scrollRestoration = 'manual';
+window.history.scrollRestoration = 'auto';
