8cc268d8dfea08a87c3aa225c39bdcb03441a366
Ryan-Zayne <phoenixastro97@gmail.com>
2023-08-27 16:10:06 +0100

    .

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,7 @@ import { logo } from '../assets/brand';
 function Logo({ className = '' }: { className?: string }) {
 	return (
 		<img
-			className={twMerge(`min-h-[4.47rem] w-[13rem] md:min-h-[5.5rem] md:w-[16rem] ${className}`)}
+			className={twMerge(`h-[4.47rem] w-[13rem] md:h-[5.5rem] md:w-[16rem] ${className}`)}
 			src={logo}
 			alt=""
 		/>
