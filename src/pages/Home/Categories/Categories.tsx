bac085cba1d95e87c8bd4c298b7ee3b4d590030c
Ryan-Zayne <phoenixastro97@gmail.com>
2023-08-27 17:05:52 +0100

    .

--- src/pages/Home/Categories/Categories.tsx
+++ src/pages/Home/Categories/Categories.tsx
@@ -8,6 +8,7 @@ const categories = [
 		title: 'SmartPhones',
 		path: '/smartphones',
 		image: 'https://res.cloudinary.com/djvestif4/image/upload/v1685436586/smartphone-transformed_jbfngh_t4v6hj.webp',
+		imageAspectRatio: 'aspect-[0.83]',
 		bg_light: 'bg-orange-400',
 		bg_dark: 'bg-[hsl(27,96%,33%)]',
 	},
@@ -15,6 +16,7 @@ const categories = [
 		title: 'Laptops',
 		path: '/laptops',
 		image: 'https://res.cloudinary.com/djvestif4/image/upload/v1685436585/laptop-transformed_dhamlu_dmts1f.webp',
+		imageAspectRatio: 'aspect-[1.33]',
 		bg_light: 'bg-gray-400',
 		bg_dark: 'bg-[hsl(200,6%,31%)]',
 	},
@@ -22,6 +24,7 @@ const categories = [
 		title: 'Vehicles',
 		path: '/vehicles',
 		image: 'https://res.cloudinary.com/djvestif4/image/upload/v1685436585/car-transformed_wegeou.webp',
+		imageAspectRatio: 'aspect-[2.02]',
 		bg_light: 'bg-purple-400',
 		bg_dark: 'bg-[hsl(270,95%,25%)]',
 	},
@@ -29,6 +32,7 @@ const categories = [
 		title: 'Watches',
 		path: '/watches',
 		image: 'https://res.cloudinary.com/djvestif4/image/upload/v1685436588/watches-transformed_tgsflz.webp',
+		imageAspectRatio: 'aspect-[0.98]',
 		bg_light: 'bg-cyan-400',
 		bg_dark: 'bg-[hsl(188,86%,38%)]',
 	},
@@ -36,6 +40,7 @@ const categories = [
 		title: 'Digital Lighting',
 		path: '/lighting',
 		image: 'https://res.cloudinary.com/djvestif4/image/upload/v1685436587/lighting-transformed_bzmi3h.webp',
+		imageAspectRatio: 'aspect-[1.03]',
 		bg_light: 'bg-green-300',
 		bg_dark: 'bg-[hsl(151,76%,26%)]',
 	},
@@ -66,7 +71,7 @@ function Categories() {
 				</Link>
 			</div>
 			<div className="flex w-[12rem] items-center lg:w-[15rem]">
-				<img className="h-[8rem]" src={category.image} loading="lazy" alt="" />
+				<img className={`${category.imageAspectRatio}`} src={category.image} loading="lazy" alt="" />
 			</div>
 		</li>
 	));
