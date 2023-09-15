02688f25365e8ecf1755ce544be85ae16f4a5864
Ryan-Zayne <phoenixastro97@gmail.com>
2023-09-15 23:35:48 +0100

    .

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,3 +1,4 @@
+import type { IconType } from 'react-icons';
 import { BiSearchAlt2 } from 'react-icons/bi';
 import { twMerge } from 'tailwind-merge';
 import Button, { type ButtonProps } from './Button';
@@ -5,7 +6,7 @@ import Button, { type ButtonProps } from './Button';
 type SearchFormProps = Pick<ButtonProps, 'theme' | 'variant' | 'size' | 'text' | 'className'> & {
 	btnClassName?: string;
 	inputClassName?: string;
-	buttonIcon?: JSX.Element;
+	buttonIcon?: React.ComponentType | IconType;
 	placeholder?: string;
 };
 
@@ -14,7 +15,7 @@ function SearchForm(props: SearchFormProps) {
 		className = '',
 		btnClassName = '',
 		inputClassName = '',
-		buttonIcon = <BiSearchAlt2 />,
+		buttonIcon: ButtonIcon = BiSearchAlt2,
 		theme = 'secondary',
 		variant = 'input',
 		size = 'sm',
@@ -26,7 +27,7 @@ function SearchForm(props: SearchFormProps) {
 		<form className={twMerge(`flex items-center ${className}`)} onSubmit={(e) => e.preventDefault()}>
 			<input
 				className={twMerge(
-					`w-full rounded-[2.5rem_0_0_2.5rem] border-secondary bg-[--color-body,_white] py-[0.6rem] pl-[2.3rem] transition-[box-shadow] duration-200 [border-width:2px_0_2px_2px] placeholder:font-[500] placeholder:text-placeholder focus-within:shadow-[1px_0_10px_2px_var(--color-secondary)] max-sm:placeholder:text-[1.4rem] ${inputClassName}`
+					`w-full rounded-[2.5rem_0_0_2.5rem] border-secondary bg-[--color-body,_white] py-[0.6rem] pl-[2.3rem] transition-[box-shadow] duration-200 [border-width:2px_0_2px_2px] placeholder:font-[500] placeholder:text-placeholder focus-within:box-shadow-[1px_0_10px_2px_var(--color-secondary)] max-sm:placeholder:text-[1.4rem] ${inputClassName}`
 				)}
 				type="search"
 				name="Search"
@@ -41,7 +42,7 @@ function SearchForm(props: SearchFormProps) {
 				theme={theme}
 				size={size}
 			>
-				{text ?? buttonIcon}
+				{text ?? <ButtonIcon />}
 			</Button>
 		</form>
 	);
